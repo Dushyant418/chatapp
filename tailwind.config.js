1167741cf84be721acda212a34afe2c6aeeb7961
Dushyant418 <dushyantlochab335@gmail.com>
2024-01-15 10:46:01 +0530

    _add font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
